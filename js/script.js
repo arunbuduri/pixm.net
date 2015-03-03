9a42dc0d994fea98bda03942395600b154a5d250
Mikeumus <mikeumus@gmail.com>
2015-03-03 16:07:46 -0500

    Stats on scroll and slideshow splash landing.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/*jshint jquery:true */
+/*jshint unused:false */
+
+$(document).ready(function($) {
+  "use strict";
+
 /* Kimono is too slow
 $.ajax({
   url:"https://www.kimonolabs.com/api/deu8kwmq?apikey=KNqzpTaPqOfXhtTbkZVcvq8s9rn9xjqq",
@@ -19,3 +25,22 @@ $.ajax({
 //});
 
 
+    /*!
+     * IE10 viewport hack for Surface/desktop Windows 8 bug
+     * Copyright 2014 Twitter, Inc.
+     * Licensed under the Creative Commons Attribution 3.0 Unported License. For
+     * details, see http://creativecommons.org/licenses/by/3.0/.
+     */
+    // See the Getting Started docs for more information:
+    // http://getbootstrap.com/getting-started/#support-ie10-width
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement('style');
+        msViewportStyle.appendChild(
+            document.createTextNode(
+                '@-ms-viewport{width:auto!important}'
+            )
+        );
+        document.querySelector('head').appendChild(msViewportStyle);
+    }
+
+}(jQuery));
